255d44c5de1e92f65bf51a180fb11abae1bb8ebb
Nayana Hettiarachchi <nayana@corp-gems.com>
2018-07-25 04:43:48 +0700

    first commit

---
+++ public/js/data.json.js
@@ -0,0 +1,66 @@
+var data = {
+    "count": 10785236,
+    "photos": [{
+        "photo_id": 27932,
+        "photo_title": "Atardecer en Embalse",
+        "photo_url": "http://www.panoramio.com/photo/27932",
+        "photo_file_url": "http://mw2.google.com/mw-panoramio/photos/medium/27932.jpg",
+        "longitude": 79.8612,
+        "latitude": 6.9271,
+        "width": 500,
+        "height": 375,
+        "upload_date": "25 June 2006",
+        "owner_id": 4483,
+        "owner_name": "Miguel Coranti",
+        "owner_url": "http://www.panoramio.com/user/4483"
+    },
+    {
+        "photo_id": 522084,
+        "photo_title": "In Memoriam Antoine de Saint Exupéry",
+        "photo_url": "http://www.panoramio.com/photo/522084",
+        "photo_file_url": "http://mw2.google.com/mw-panoramio/photos/medium/522084.jpg",
+        "longitude": 79.9777256,
+        "latitude": 6.9299869,
+        "width": 500,
+        "height": 350,
+        "upload_date": "21 January 2007",
+        "owner_id": 109117,
+        "owner_name": "Busa Péter",
+        "owner_url": "http://www.panoramio.com/user/109117"
+    }]
+};
+
+var land = {
+    "land": [
+        {
+            "road" : "stanley thilakarathna",
+            "area" : "Nuegoda",
+            "land" : 20,
+            "price": "2,700,000"
+        },
+        {
+            "road" : "pagoda road",
+            "area" : "Nuegoda",
+            "land" : 20,
+            "price": "2,500,000"
+        },
+        {
+            "road" : "Minuwangoda",
+            "area" : "Minuwangoda",
+            "land" : 12,
+            "price": "4,600,000"
+        },
+        {
+            "road" : "Keppetipola Mawatha",
+            "area" : "Gampaha",
+            "land" : 20,
+            "price": "6,500,000"
+        },
+        {
+            "road" : "wijerama road",
+            "area" : "Nuegoda",
+            "land" : 8.5,
+            "price": "31,500,000"
+        }
+    ]
+};
