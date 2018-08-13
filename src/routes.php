c8905e2386f00e92c05675a7aba5ab0130a745c1
Nayana Hettiarachchi <nayana@corp-gems.com>
2018-08-13 17:10:40 +0700

    init

--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,19 @@ use Slim\Http\Response;
 
 // Routes
 
+$app->get("/data", function (Request $request, Response $response, array $args) {
+   $json = file_get_contents("http://pituwa:pituwa@localhost:5984/market/_design/property/_view/pppcolombo");
+   $j = json_decode($json, true);
+   $x = array_map(function($val) {
+        return  ['area' => $val['value'][0], 'ppp' => $val['value'][1]];
+   }, $j['rows']);
+
+   $x = array_slice($x, 0, 10);
+
+   return $response->withJson($x);
+
+});
+
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
